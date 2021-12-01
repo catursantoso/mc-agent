bfe695e8074d40f00ab92d475a943b032cc37232
Catur Santoso <89720862+makassardivecenter@users.noreply.github.com>
2021-12-01 11:32:37 +0800

    perubahan
    menambahkan tabel agent dan fungsi crud

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from flask import Flask, render_template, url_for, redirect, session, request
 import firebase_admin
 from firebase_admin import credentials, firestore
 from flask.helpers import flash
+from flask.templating import render_template_string
 from werkzeug.security import generate_password_hash, check_password_hash
 from functools import wraps
 
@@ -13,6 +14,7 @@ db = firestore.client()
 app = Flask(__name__)
 app.secret_key = 'makeitsecret'
 
+# fungsi untuk login required
 def login_required(f):
   @wraps(f)
   def wrapper(*args , **kwargs):
@@ -23,6 +25,22 @@ def login_required(f):
       return redirect(url_for('login'))
   return wrapper
 
+# fungsi untuk admin required
+def admin_required(f):
+  @wraps(f)
+  def wrapper(*args , **kwargs):
+    if 'user' in session:
+      if session['user']['access'] == 'admin':
+        return f(*args, **kwargs)
+      else:
+        flash('Maaf anda bukan admin', 'danger')
+        return redirect(url_for('dashboard'))
+    else:
+      flash('Maaf anda belum login', 'danger')
+      return redirect(url_for('login'))
+  return wrapper
+
+# login
 @app.route('/login', methods=['GET', 'POST'])
 def login():
   if request.method == 'POST':
@@ -47,6 +65,7 @@ def login():
       return redirect(url_for('login'))
   return render_template('login.html')
 
+# register
 @app.route('/register', methods = ['GET', 'POST'])
 def register():
   if request.method == 'POST':
@@ -80,32 +99,74 @@ def register():
     return redirect(url_for('login'))
   return render_template('register.html')
 
+# logout
 @app.route('/logout')
 def logout():
   session.clear()
   return render_template('login.html')
 
+# dashboard
 @app.route('/dashboard')
 @login_required
 def dashboard():
   return render_template('dashboard.html')
 
+# index
 @app.route('/')
 def index():
   return render_template('index.html')
 
+# agent
+@app.route('/agent')
+@login_required
+@admin_required
+def agent():
+  agent = db.collection("users").stream()
+  ag = []
+  for agt in agent:
+    a = agt.to_dict()
+    a["id"] = agt.id
+    ag.append(a)
+  return render_template('agent.html', data=ag)
+
+# ubah agent
+@app.route('/agent/ubah/<uid>', methods = ['GET', 'POST'])
+def ubah_agent(uid):
+  if request.method == 'POST':
+    data = {
+      'name': request.form['name'],
+      'username': request.form['username'],
+      'access': request.form['access']
+    }
+    db.collection('users').document(uid).set(data, merge = True)
+    flash('Selamat! data anda berhasil di ubah', 'primary')
+    return redirect(url_for('agent'))
+  user = db.collection('users').document(uid).get().to_dict()
+  user['id'] = uid
+  return render_template('ubah_agent.html', user=user)
+
+# hapus data agent
+@app.route('/agent/hapus/<uid>')
+def hapus_agent(uid):
+  db.collection('users').document(uid).delete()
+  flash('Data agent berhasil di hapus', 'danger')
+  return redirect(url_for('agent'))
+
 @app.route('/buttons')
 @login_required
+@admin_required
 def buttons():
   return render_template('buttons.html')
 
 @app.route('/cards')
 @login_required
+@admin_required
 def cards():
   return render_template('cards.html')
 
 @app.route('/error')
 @login_required
+@admin_required
 def error():
   return render_template('404.html')
 
